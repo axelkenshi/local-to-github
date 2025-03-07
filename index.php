7c318c8f71dd8873e7b283c1be6294bb4c2c65d7
axel <nshxel@gmail.com>
2025-03-07 20:12:44 +0700

    menambah kode

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
   <h1>send</h1>
   <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia eligendi necessitatibus reiciendis incidunt beatae a maiores mollitia, fugit ducimus vitae dolor neque blanditiis cum molestiae nam eos porro architecto, totam accusantium expedita quaerat delectus, aliquam alias? Neque eligendi eaque et sed, cumque repudiandae quae optio quos maiores, doloribus quibusdam alias?</p>
+  <table>
+    <th>test</th>
+    <td>hai</td>
+  </table>
 </body>
 </html>
